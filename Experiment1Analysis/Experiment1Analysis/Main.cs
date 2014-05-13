867f900e6a00800db99c006b315fcf3d92bde3ef
Mick Neupart <mickneupart@gmail.com>
2014-05-13 15:40:37 +0200

    added GetLogisticRegressionCSV

--- Experiment1Analysis/Experiment1Analysis/Main.cs
+++ Experiment1Analysis/Experiment1Analysis/Main.cs
@@ -19,7 +19,7 @@ namespace Experiment1Analysis
 			}
 			else
 			{
-				basePath = "/Users/Thorbjorn/Dropbox/MED6/Results/Experiment1Participants";
+				basePath = "/Users/WorkMachine/Dropbox/MED6 - Dropbox/MED6/Results/Experiment1Participants";
 				//basePath = "/Users/WorkMachine/Dropbox/MED6 - Dropbox/MED6/Results/Experiment1Participants";
 				Console.WriteLine("Using default base path " + basePath);
 			}
@@ -46,6 +46,32 @@ namespace Experiment1Analysis
 			experiment.PresentParticipant(31);		
 			
 			Console.WriteLine(experiment.QuickStats());
+
+			GetLogisticRegressionCSV(basePath,experiment);
+		}
+
+		static void GetLogisticRegressionCSV(string path, ExperimentData experiment)
+		{
+			List<float> respons = new List<float>();
+			List<float> stimuli = new List<float>();
+			//Get data for logistic regression
+			foreach(Participant p in experiment.participants)
+			{
+				foreach(Trial t in p.trials)
+				{
+					foreach(Observation o in t.observations)
+					{
+						if(o.response == -1)
+							respons.Add(0);
+						else
+							respons.Add(o.response);
+						
+						stimuli.Add(o.stimulus);
+					}
+				}
+			}
+			
+			CSVWriter.Write(Path.Combine(path,"output/logRegress.csv"),"respons",respons.ToArray(),"stimuli",stimuli.ToArray());
 		}
 
 		static void WriteTrialsByFirstResponse (string basePath, ExperimentData experiment)
